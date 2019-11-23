21ca89e0ba08e54966349db473f4a7ef2345f3ab
DerrickLHoumey <55327123+DerrickLHoumey@users.noreply.github.com>
2019-11-23 10:16:14 -0600

    Initial commit

---
+++ functionsPractice.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 16, 2019
+
+@author: ITAUser
+'''
+def addition(num1, num2):
+    sumOfTwoNumbers = num1 + num2
+    return sumOfTwoNumbers
+
+sumOfTwoNumbers = addition(2,5)
+
+def sum_three_numbers(num1,num2,num3):
+    sumOfThreeNumbers = num1+num2+num3
+    return sumOfThreeNumbers
+
+sumOfThreeNumbers = sum_three_numbers(5,4,1)
